2fcf5fa093ee3b3c5c3aeb5f0f465bfe331a4c35
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-08 10:56:13 -0300

    10 - excluir.cpp

--- ponteiros/10 - excluir.cpp
+++ ponteiros/10 - excluir.cpp
@@ -1,3 +1,28 @@
+/* Faça um programa que receba o tamanho N de um vetor e o preencha. Encontre o segundo menor e o segundo maior elementos, reduza o vetor em duas posições e preencha o novo vetor com os demais elementos, excluindo os dois elementos identificados. Imprima o novo vetor.
+
+Entradas:
+
+    int n - Tamanho do vetor.
+    float vet[n] - Vetor que será preenchido.
+
+Saídas:
+
+    Novo vetor, retirando o segundo menor e segundo maior elementos (float).
+
+
+Exemplos de Entradas e Saídas:
+
+Entradas:
+4
+3
+1
+6
+5
+
+Saídas:
+1
+6*/
+
 #include <iostream>
 
 using namespace std;
