1bb26befe19f77178f06ce5b1fb9965db4627352
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-08 10:52:48 -0300

    multiplos.cpp

--- ponteiros/multiplos.cpp
+++ ponteiros/multiplos.cpp
@@ -1,3 +1,33 @@
+/*
+
+Faça um programa que receba a quantidade de números que o usuário digitará e em seguida preencha um vetor com os números inteiros, calcule e mostre: os números múltiplos de 2; os números múltiplos de 3; e os números múltiplos de 2 e 3. Os números múltiplos de 2 e 3 podem aparecer nos casos isolados. Caso não exista nenhum número em cada caso, o programa deve retornar 0 na respectiva saída.
+
+Observação: o vetor deverá ser alocado dinamicamente
+
+Entradas:
+
+    Quantidade de números a serem inseridos pelo usuário/li>
+    Sequência de números inteiros
+
+Saídas:
+
+    Números que são múltiplos de 2;
+    Números que são múltiplos de 3;
+    Números que são múltiplos de 2 e 3 simultaneamente.
+
+Exemplo de entrada:
+
+7
+4 6 3 9 7 10 13
+
+Exemplo de saída:
+
+4 6 10
+6 3 9 
+6
+
+*/
+
 #include <iostream>
 using namespace std;
 
