22c047ab1ffc0bc50f0f6fffc13174e50f9d88a0
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-08 10:43:08 -0300

    Update registroCarro.cpp

--- ponteiros/registroCarro.cpp
+++ ponteiros/registroCarro.cpp
@@ -1,3 +1,37 @@
+/*
+
+Faça um programa que crie um registro Carro com os campos: marca, ano, distância percorrida na última viagem (em quilômetros) e consumo de combustível na última viagem (em litros). Seu programa deverá alocar dinamicamente uma variável do tipo Carro e ler, a partir de um arquivo texto chamado "entrada.txt", as informações de um carro em particular. Em seguida, faça um subprograma que receba como entrada as informações lidas e retorne à função principal quantos quilômetros por litro o carro em questão realizou em sua última viagem. As informações da marca, ano do carro, consumo de combustível na última viagem (em litros) e o resultado obtido pela função, devem ser gravados em um outro arquivo chamado "saida.txt". Assuma que o carro sempre terá consumido alguma quantidade de combustível.
+
+Entradas (conteúdo do arquivo):
+
+    Cadeia de caracteres representando a marca de um carro (sem espaços).
+    Inteiro indicando o ano de fabricação do carro.
+    Ponto flutuante indicando a distância percorrida pelo carro em sua última viagem.
+    Ponto flutuante indicando o consumo realizado pelo carro em sua última viagem.
+
+Saída (conteúdo do arquivo):
+
+    Cadeia de caracteres representando a marca de um carro (sem espaços).
+    Inteiro indicando o ano de fabricação do carro.
+    Ponto flutuante indicando o consumo realizado pelo carro em sua última viagem.
+    Ponto flutuante indicando quantos quilômetros por litro o carro realizou em sua última viagem.
+
+Exemplo de Entrada (conteúdo do arquivo):
+
+palio
+2008
+62.5
+5.3
+
+Exemplo de Saída (conteúdo do arquivo):
+
+palio
+2008
+5.3
+11.79
+
+*/
+
 #include <iostream>
 #include <fstream>
 
