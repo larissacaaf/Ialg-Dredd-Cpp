1cf040fc3222db22d15dcb1570ee8eb06da636a8
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-08 10:55:00 -0300

    media.cpp

--- ponteiros/media.cpp
+++ ponteiros/media.cpp
@@ -1,3 +1,33 @@
+/*
+
+Faça um programa que leia um conjunto de números inteiros positivos até que seja lido o valor -1. Esses valores devem ser armazenados em um vetor com tamanho inicial igual a 2. Sempre que necessário o vetor deve aumentar de tamanho em 2 posições. Após a leitura de todos os valores, seu programa imprimir o valor do elemento do meio do vetor dividido por 2. Caso a quantidade de valores lidos seja par, seu programa deve calcular a média entre os elementos do meio.
+
+Entradas:
+
+    Valores inteiros positivos até que seja digitado o valor -1
+
+Saídas:
+
+    Elemento do meio dividido por 2 (ou média entre os elementos do meio).
+
+Exemplo de Entrada:
+
+4 2 19 7 8 -1
+
+Exemplo de Saída:
+
+9.5
+
+Exemplo de Entrada:
+
+7 4 6 9 -1
+
+Exemplo de Saída:
+
+5
+
+*/
+
 #include <iostream>
 using namespace std;
 
