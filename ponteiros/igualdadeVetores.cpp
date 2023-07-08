3bb5e517191e0182bc8c7f5fb841f847c6f3bd4b
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-08 10:52:00 -0300

    igualdadeVetores.cpp

--- ponteiros/igualdadeVetores.cpp
+++ ponteiros/igualdadeVetores.cpp
@@ -1,3 +1,35 @@
+/*
+
+Dois vetores são considerados iguais se ambos contiverem o mesmo conjunto de elementos, embora as permutações de elementos possam ser diferentes.
+
+Faça um programa para receba o tamanho N de dois vetores A e B de igual tamanho e descobra se os vetores são iguais ou não.
+
+Se forem iguais imprima "IGUAIS", caso contrário imprima "DIFERENTES".
+
+Observação: não haverá elementos repetidos em cada vetor.
+
+Exemplo de Entrada:
+
+5
+1 2 3 4 5
+5 4 3 2 1
+
+Exemplo de Saída:
+
+IGUAIS
+
+Exemplo de Entrada:
+
+5
+1 2 3 6 5
+5 4 3 2 1
+
+Exemplo de Saída:
+
+DIFERENTES
+
+*/
+
 #include <iostream>
 using namespace std;
 
