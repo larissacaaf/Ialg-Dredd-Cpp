e3cabbe67ef63a3021dcb045a9850f5d18f2a31a
Larissa Caaf <94768333+larissacaaf@users.noreply.github.com>
2023-07-14 22:10:50 -0300

    Update 1-logBase3.cpp

--- recursividade-buscaBinaria/1-logBase3.cpp
+++ recursividade-buscaBinaria/1-logBase3.cpp
@@ -1,3 +1,26 @@
+/*
+Faça um programa que calcula o logaritmo na base 3 de uma potência de 3. Para tanto, divisões sucessivas deverão ser utilizadas. O programa deve conter um subprograma 
+recursivo que recebe um número natural e retorna um número natural para fazer o cálculo do logaritmo.
+
+O subprograma que faz o cálculo do logaritmo não pode conter nenhuma operação de leitura nem de escrita. Não é permitido o uso da biblioteca cmath.
+
+Entradas:
+
+    um número natural que é potência de 3.
+
+Saídas:
+
+    o logaritmo do número lido.
+
+Exemplo de Entrada:
+
+243
+
+Exemplo de Saída:
+
+5
+*/
+
 #include <iostream>
 #include <cmath>
 using namespace std;
